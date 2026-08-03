b5b012fa649d28fc2d888dad21b49dad4736303b
Shireff Nady <shireffn369@gmail.com>
2026-08-03 15:21:45 +0300

    fix: log pool accept/reject response for alephium submits

--- miner/miner_gpu.py
+++ miner/miner_gpu.py
@@ -615,7 +615,7 @@ class AlephiumMiningSession:
             return False, "", ""
         import os as _os
         nonce_full_24 = self._last_nonce.to_bytes(24, "big")
-        mode = _os.environ.get("PEARL_NONCE_SUBMIT_MODE", "counter6")
+        mode = _os.environ.get("PEARL_NONCE_SUBMIT_MODE", "full24")
         # Full nonce layout: zeros(16) + extranonce(2) + counter(6)
         # bytes[16:18] = extranonce, bytes[18:24] = counter
         if mode == "counter6":
